d063cd29babfad52165fdd021fd5e0943fb10899
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-11-14 23:57:31 +0530

    cp2 dialog

--- src/components/boards/CreateNewKanbanBoard.tsx
+++ src/components/boards/CreateNewKanbanBoard.tsx
@@ -3,6 +3,8 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
 
 const CreateNewKanbanBoard = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -37,5 +39,49 @@ const CreateNewKanban = ({
 }: {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
-  return <div>CreateNewKanban</div>;
+  const [kanbanName, setKanbanName] = useState("");
+  const [kanbanDescription, setKanbanDescription] = useState("");
+
+  return (
+    <div className="mt-4 flex flex-col gap-4">
+      <Input
+        placeholder="Name of kanban board"
+        value={kanbanName}
+        onChange={(e) => {
+          setKanbanName(e.target.value);
+        }}
+      />
+      <DescriptionInput
+        value={kanbanDescription}
+        setKanbanDescription={setKanbanDescription}
+      />
+      <Button
+        onClick={() => {
+          //
+        }}
+      >
+        Create
+      </Button>
+    </div>
+  );
+};
+
+const DescriptionInput = ({
+  value,
+  setKanbanDescription,
+}: {
+  value: string;
+  setKanbanDescription: Dispatch<SetStateAction<string>>;
+}) => {
+  return (
+    <Textarea
+      value={value}
+      onChange={(e) => {
+        setKanbanDescription(e.target.value);
+      }}
+      minRows={4}
+      maxRows={8}
+      placeholder="Add a description"
+    ></Textarea>
+  );
 };
